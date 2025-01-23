d6367e540d32c358154649d9ddc8d1ad5f5c02f0
HotPotatoC <juandotulung@gmail.com>
2025-01-23 23:58:52 +0700

    refactor: moved get roadmap by slug output mapping

--- internal/backend/get_roadmap_by_slug.go
+++ internal/backend/get_roadmap_by_slug.go
@@ -3,7 +3,6 @@ package backend
 import (
 	"context"
 
-	"github.com/roadmap-thesis/backend/internal/domain"
 	"github.com/roadmap-thesis/backend/internal/io"
 )
 
@@ -20,56 +19,8 @@ func (b *backend) GetRoadmapBySlug(ctx context.Context, slug string) (io.GetRoad
 
 	roadmap.SetCreator(account)
 
-	return b.makeGetRoadmapBySlugOutput(roadmap), nil
-}
+	output := new(io.GetRoadmapBySlugOutput)
+	output.Make(roadmap)
 
-func (b *backend) makeGetRoadmapBySlugOutput(roadmap domain.Roadmap) io.GetRoadmapBySlugOutput {
-	output := io.GetRoadmapBySlugOutput{
-		ID:          roadmap.ID,
-		Title:       roadmap.Title,
-		Slug:        roadmap.Slug,
-		Description: roadmap.Description,
-		Creator: io.GetRoadmapBySlugOutputCreator{
-			ID:   roadmap.Account.ID,
-			Name: roadmap.Account.Name,
-		},
-		TotalTopics:          roadmap.TotalTopics(),
-		CompletionPercentage: roadmap.CompletionPercentage(),
-		CreatedAt:            roadmap.CreatedAt,
-		UpdatedAt:            roadmap.UpdatedAt,
-	}
-
-	// Map topics to output also map subtopics into topics
-	topicMap := make(map[int][]io.GetRoadmapBySlugOutputTopics)
-
-	for _, topic := range roadmap.Topics {
-		outputTopic := io.GetRoadmapBySlugOutputTopics{
-			ID:          topic.ID,
-			RoadmapID:   topic.RoadmapID,
-			ParentID:    topic.ParentID,
-			Title:       topic.Title,
-			Slug:        topic.Slug,
-			Description: topic.Description,
-			Order:       topic.Order,
-			Finished:    topic.Finished,
-			CreatedAt:   topic.CreatedAt,
-			UpdatedAt:   topic.UpdatedAt,
-		}
-
-		topicMap[topic.ParentID] = append(topicMap[topic.ParentID], outputTopic)
-	}
-
-	var buildTopics func(parentID int) []io.GetRoadmapBySlugOutputTopics
-	buildTopics = func(parentID int) []io.GetRoadmapBySlugOutputTopics {
-		var outputTopics []io.GetRoadmapBySlugOutputTopics
-		for _, topic := range topicMap[parentID] {
-			topic.Subtopics = buildTopics(topic.ID)
-			outputTopics = append(outputTopics, topic)
-		}
-		return outputTopics
-	}
-
-	output.Topics = buildTopics(0)
-
-	return output
+	return *output, nil
 }
--- internal/io/get_roadmap_io.go
+++ internal/io/get_roadmap_io.go
@@ -1,6 +1,10 @@
 package io
 
-import "time"
+import (
+	"time"
+
+	"github.com/roadmap-thesis/backend/internal/domain"
+)
 
 type GetRoadmapBySlugOutput struct {
 	ID                   int                            `json:"id"`
@@ -13,6 +17,52 @@ type GetRoadmapBySlugOutput struct {
 	CompletionPercentage float64                        `json:"completion_percentage"`
 	CreatedAt            time.Time                      `json:"created_at"`
 	UpdatedAt            time.Time                      `json:"updated_at"`
+}
+
+func (o *GetRoadmapBySlugOutput) Make(roadmap domain.Roadmap) {
+	o.ID = roadmap.ID
+	o.Title = roadmap.Title
+	o.Slug = roadmap.Slug
+	o.Description = roadmap.Description
+	o.Creator = GetRoadmapBySlugOutputCreator{
+		ID:   roadmap.Account.ID,
+		Name: roadmap.Account.Name,
+	}
+	o.TotalTopics = roadmap.TotalTopics()
+	o.CompletionPercentage = roadmap.CompletionPercentage()
+	o.CreatedAt = roadmap.CreatedAt
+	o.UpdatedAt = roadmap.UpdatedAt
+
+	topicMap := make(map[int][]GetRoadmapBySlugOutputTopics)
+
+	for _, topic := range roadmap.Topics {
+		outputTopic := GetRoadmapBySlugOutputTopics{
+			ID:          topic.ID,
+			RoadmapID:   topic.RoadmapID,
+			ParentID:    topic.ParentID,
+			Title:       topic.Title,
+			Slug:        topic.Slug,
+			Description: topic.Description,
+			Order:       topic.Order,
+			Finished:    topic.Finished,
+			CreatedAt:   topic.CreatedAt,
+			UpdatedAt:   topic.UpdatedAt,
+		}
+
+		topicMap[topic.ParentID] = append(topicMap[topic.ParentID], outputTopic)
+	}
+
+	var buildTopics func(parentID int) []GetRoadmapBySlugOutputTopics
+	buildTopics = func(parentID int) []GetRoadmapBySlugOutputTopics {
+		var outputTopics []GetRoadmapBySlugOutputTopics
+		for _, topic := range topicMap[parentID] {
+			topic.Subtopics = buildTopics(topic.ID)
+			outputTopics = append(outputTopics, topic)
+		}
+		return outputTopics
+	}
+
+	o.Topics = buildTopics(0)
 }
 
 type GetRoadmapBySlugOutputCreator struct {
